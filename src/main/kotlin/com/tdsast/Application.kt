404e7a0e33b7556bdf0ee59636640b610fac76e8
Taoning Ge <gtn1024o@outlook.com>
2022-06-21 19:27:00 +0800

    init

---
+++ src/main/kotlin/com/tdsast/Application.kt
@@ -0,0 +1,14 @@
+package com.tdsast
+
+import io.ktor.server.application.*
+import com.tdsast.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+    configureSecurity()
+    configureSerialization()
+}
